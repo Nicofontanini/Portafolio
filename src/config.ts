2420fc720ac675ea70f178d828b096a2a0f85a79
Nicofontanini <anicolasfontanini@gmail.com>
2024-06-24 14:42:07 -0300

    primera actualizacion

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'CV Nicolas Fontanini';
+export const SITE_DESCRIPTION = 'Mi portafolio personal como desarrollador Web.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
